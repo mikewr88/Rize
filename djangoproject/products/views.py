f15f94e3f38c704fec90780e76313dfb35858503
Michael Gloudeman <mikewr88@yahoo.com>
2019-05-24 12:47:15 -0700

    angular init

--- djangoproject/products/views.py
+++ djangoproject/products/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from products')
+    # render(request, template_name, context=None, content_type=None, status=None, using=None). Here we are just using template name.
+    # This Combines a given template with a given context dictionary and returns an HttpResponse object with that rendered text.
     return render(request, 'products/index.html')
