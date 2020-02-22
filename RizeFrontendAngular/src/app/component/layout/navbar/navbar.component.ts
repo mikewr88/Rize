b1efa278c0b74f426061447a0beaa2db6cebc9e7
Michael Gloudeman <mikewr88@yahoo.com>
2020-02-21 23:17:15 -0800

    navbar styling and js beginning

--- RizeFrontendAngular/src/app/component/layout/navbar/navbar.component.ts
+++ RizeFrontendAngular/src/app/component/layout/navbar/navbar.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent implements OnInit {
+
 
   constructor() { }
 
   ngOnInit() {
+
+      var navpiece = document.querySelector(".nav-piece2");
+      document.addEventListener("scroll", (scrollevent)=> {
+
+        if (window.scrollY <= 100){
+          navpiece.style.top = (100 - window.scrollY) + "px";
+        }else{
+          navpiece.style.top = "0px";
+
+        }
+      })
+
+
   }
 
 }
