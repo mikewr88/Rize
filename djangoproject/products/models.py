e514cce10600f335e7a0ddf9864a521289e859eb
Michael Gloudeman <mikewr88@yahoo.com>
2020-02-25 14:59:30 -0800

    Products component design begin, products model updated to include imgurl (migration made)

--- djangoproject/products/models.py
+++ djangoproject/products/models.py
@@ -6,3 +6,4 @@ class Product(models.Model):
     description= models.CharField(max_length=255)
     price= models.DecimalField(max_digits=4, decimal_places=2)
     created_at= models.DateTimeField(auto_now_add=True)
+    imgurl=models.CharField(max_length=255, null = True)
